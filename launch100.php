ba263d2f6835f8995aa65fa709cdace54bf1772f
Roy Tokeshi <roy.tokeshi@gmail.com>
2020-06-15 20:50:36 -0700

    100

---
+++ launch100.php
@@ -0,0 +1,9 @@
+<?php
+shell_exec("curl-mb3-100.sh");
+header('Location: http://mbiii.r4t.net');
+?>
+
+<?php
+shell_exec("yourscript.sh");
+header('Location: http://mbiii.r4t.net/2020/06/16/congratulations/');
+?>
