559b5fc4673baf7b9fcb5e15a09f30417903e3ca
Roy Tokeshi <roy.tokeshi@gmail.com>
2020-06-25 12:27:10 -0700

    add nova paths

--- instance-load.sh
+++ instance-load.sh
@@ -6,9 +6,16 @@ curl_path="http://mbiii.r4t.net"
 curl_path_admin="http://mbiii.r4t.net/wp-admin"
 curl_path_east_admin="http://east.r4t.net/wp-admin"
 curl_path_west_admin="http://west.r4t.net/wp-admin"
+curl_path_nova_admin="http://nova.r4t.net/wp-admin"
 curl_path_east="http://east.r4t.net"
 curl_path_west="http://west.r4t.net"
+curl_path_nova="http://nova.r4t.net"
 curl_path_attack="http://mbiii.r4t.net/?s=String+userLoginQuery+%3D+++%22SELECT+user_id%2C+username%2C+password_hash+FROM+users+WHERE+username+%3D+%27%22+++%2B+request.getParameter%28%22user%22%29+%2B+%22%27%22%3B"
+curl_path_attack_east="http://east.r4t.net/?s=String+userLoginQuery+%3D+++%22SELECT+user_id%2C+username%2C+password_hash+FROM+users+WHERE+username+%3D+%27%22+++%2B+request.getParameter%28%22user%22%29+%2B+%22%27%22%3B"
+curl_path_attack_west="http://west.r4t.net/?s=String+userLoginQuery+%3D+++%22SELECT+user_id%2C+username%2C+password_hash+FROM+users+WHERE+username+%3D+%27%22+++%2B+request.getParameter%28%22user%22%29+%2B+%22%27%22%3B"
+curl_path_attack_nova="http://nova.r4t.net/?s=String+userLoginQuery+%3D+++%22SELECT+user_id%2C+username%2C+password_hash+FROM+users+WHERE+username+%3D+%27%22+++%2B+request.getParameter%28%22user%22%29+%2B+%22%27%22%3B"
+
+
 delay_span=120
 curl_range=2000
 
@@ -26,9 +33,14 @@ for i in {1..$curl_count}
  curl $curl_path_admin
  curl $curl_path_east_admin
  curl $curl_path_west_admin
+ curl $curl_path_nova_admin
  curl $curl_path_east
  curl $curl_path_west
- curl $cur_path_attack
+ curl $curl_path_nova
+ curl $curl_path_attack
+ curl $curl_path_attack_east
+ curl $curl_path_attack_west
+ curl $curl_path_attack_nova
 done
 #End of curl fork
 }&
